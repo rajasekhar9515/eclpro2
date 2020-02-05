132a19c5dab780641fe8db3464317904725d0434
rajasekhar <rajasekhar123@gmail.com>
2020-02-05 21:19:42 +0530

    eclpro2 project

---
+++ eclpro2/src/test/java/eclpro2/protest.java
@@ -0,0 +1,19 @@
+package eclpro2;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class protest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
